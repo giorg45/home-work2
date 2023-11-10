27b0e60edc5aa5b0d9be28d3607de07a2b9c18ea
giorg45 <giagorgadze45@gmail.com>
2023-11-10 02:03:03 -0800

    first commit

---
+++ .py
@@ -0,0 +1,19 @@
+name = " giorgi"
+surname = "gorgadze"
+full_name = " giorgi gorgadze "
+height =  188.5
+age = 19
+#print(name+" "+surname+" "+str(height))
+
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(age))
+print(type(full_name))
+
+
+print(full_name+" "+str(age)+" wlis "+str(height)+"cm simagle")
+
+
+
+
